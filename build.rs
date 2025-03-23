03bff77991e189941ff099b8659e4bda4a11530a
Cyber Phantom <inam123451@gmail.com>
2025-03-23 16:26:34 +0530

    Switch to prost for protobuf implementation

--- build.rs
+++ build.rs
@@ -1,11 +1,6 @@
-fn main() {
-    println!("cargo:rerun-if-changed=protos/update_metadata.proto");
+use std::io::Result;
 
-    protobuf_codegen::Codegen::new()
-        .protoc()
-        .protoc_path(&protoc_bin_vendored::protoc_bin_path().unwrap())
-        .include("protos")
-        .input("protos/update_metadata.proto")
-        .cargo_out_dir("protos")
-        .run_from_script();
+fn main() -> Result<()> {
+    prost_build::compile_protos(&["protos/update_metadata.proto"], &["protos/"])?;
+    Ok(())
 }
